7bcd8b1ec378d202a6d16870f5a91d1b027f9763
Chris Marques <test.code.debug@gmail.com>
2022-07-24 15:33:18 -0400

    Add job to populate Articles
    Defaults to getting articles for yesterday

---
+++ app/jobs/populate_job.rb
@@ -0,0 +1,26 @@
+class PopulateJob
+  include Sidekiq::Worker
+  sidekiq_options queue: 'default'
+
+  def perform(from = 1.day.ago.beginning_of_day.iso8601, to = 1.day.ago.end_of_day.iso8601)
+    api_key = 'a63afa028c594bcd97ede1c8559c04a3'
+    news = News.new(api_key)
+    page = 1
+
+    loop do
+      articles = news.get_everything(q: '"Florida Man"', sortBy: 'popularity', from: from, to: to, page: page, searchIn: 'title')
+      articles.uniq(&:title).each do |art|
+        Article.create(
+          title: art.title,
+          description: art.description,
+          url: art.url,
+          url_to_image: art.urlToImage,
+          published_at: art.publishedAt
+        )
+      end
+
+      break if articles.count < 100
+      page += 1
+    end
+  end
+end
